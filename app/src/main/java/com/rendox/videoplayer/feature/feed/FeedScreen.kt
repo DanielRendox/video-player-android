e779c2110c346e8c8b45c5c1584a6264ab975954
DanielRendox <daniel.rendox@gmail.com>
2024-09-27 18:25:19 +0200

    Add bottom padding to the feed

--- app/src/main/java/com/rendox/videoplayer/feature/feed/FeedScreen.kt
+++ app/src/main/java/com/rendox/videoplayer/feature/feed/FeedScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -93,6 +94,7 @@ private fun FeedScreenStateless(
                         .padding(contentPadding)
                         .fillMaxSize(),
                     verticalArrangement = Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(bottom = 16.dp)
                 ) {
                     items(
                         items = screenState.videos,
